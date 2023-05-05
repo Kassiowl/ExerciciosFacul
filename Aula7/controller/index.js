0681496dba7689fdab3b8e5566f184b89a590b63
Kassio Wallace <kassiowl34@gmail.com>
2023-05-05 15:13:11 -0300

    aula 7 add

---
+++ Aula7/controller/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.post('/post-test', (req, res) =>{
+    console.log('Got body:', req.body);
+    res.sendStatus(200);
+})
+
+
+
+app.listen(8080, () => console.log(`Started Server at http://localhost:8080`));
