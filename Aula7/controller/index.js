9ae3ed45e78474eab287d9efd77dbd3f4f88926b
Kassio Wallace <kassiowl34@gmail.com>
2023-05-10 14:49:24 -0300

    aula 7 add

--- Aula7/controller/index.js
+++ Aula7/controller/index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
+app.set('view engine', 'pug');
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname+'/../views/index.html'))
+})
 
 app.post('/post-test', (req, res) =>{
     console.log('Got body:', req.body);
-    res.sendStatus(200);
+    res.render(path.join(__dirname+'/../views/page_info'), { context: req.body });
 })
-
-
 
 app.listen(8080, () => console.log(`Started Server at http://localhost:8080`));
 
