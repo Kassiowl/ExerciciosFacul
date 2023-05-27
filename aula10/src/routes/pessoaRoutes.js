59380d8695be7bd3c7ef064bf578cec9f90c0a50
Kassio Wallace <kassiowl34@gmail.com>
2023-05-27 17:09:20 -0300

    aula 10 add

---
+++ aula10/src/routes/pessoaRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const pessoaController = require('../controllers/pessoaController');
+
+router.get('/pessoa/cadastrar', pessoaController.cadastrarView);
+router.post('/pessoa/cadastrar', pessoaController.cadastrarPessoa);
+
+router.get('/pessoa/listar', pessoaController.listarView);
+
+router.get('/pessoa/editar/:id', pessoaController.editarView);
+router.post('/pessoa/editar', pessoaController.editarPessoa);
+router.post('/pessoa/deletar', pessoaController.deletarPessoa);
+
+module.exports = router;
